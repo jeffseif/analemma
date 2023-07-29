393b58fbe3842774afa5a725ea8e38ef9ce5b298
Jeffrey Edwin Seifried <jeffrey.seifried@gmail.com>
2023-07-29 10:55:59 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = "Jeffrey Seifried"
 __email__ = "jeffrey.seifried@gmail.com"
 __program__ = "analemma"
 __url__ = "http://github.com/jeffseif/{}".format(__program__)
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 setup(
